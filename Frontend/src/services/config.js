45e0fc373387870afb5c3215bbb09b6dfe09a0c5
vtsappsteam <vtsappsteam@gmail.com>
2025-08-21 14:40:54 +0200

    pipeline

--- Frontend/src/services/config.js
+++ Frontend/src/services/config.js
@@ -1,7 +1,25 @@
 import axios from 'axios'
- 
+
+// Odredi baseURL u zavisnosti od mode
+let baseURL = ''
+
+
+let okruzenje = "production";
+
+
+
+if (okruzenje === 'development') {
+  // Development URL
+  baseURL = 'http://localhost:1000'
+} else {
+  // Production URL (nakon build-a)
+  baseURL = 'http://160.99.40.212:1000'
+}
+
+console.log('Axios baseURL:', baseURL)
+
 const api = axios.create({
-  baseURL: 'http://160.99.40.212:1000',
+  baseURL: baseURL,
   timeout: 5000
 })
 
